4c0bd259a8fe5a4cf2cf328654b99850374f0dd6
Nick Moriarty <njmoriarty@gmail.com>
2021-07-22 19:31:23 +0100

    Set up LZSS as a delegator

--- lib/layoutable/lzss_data.rb
+++ lib/layoutable/lzss_data.rb
@@ -2,20 +2,23 @@
 
 require 'lzss_decompressor'
 require 'stringio'
+require 'delegate'
 
 module Layoutable
   # Wrapper to return the contained structure
-  module LZSSData
-    module_function
-
-    def new(data, *rowinfo)
+  class LZSSData < SimpleDelegator
+    def initialize(data, *rowinfo)
       # NB: Duplicate code from Container#initialize
       rowclass = rowinfo.first.is_a?(String) ? Struct : rowinfo.shift
       rowargs = rowinfo
+      io = StringIO.new(data)
+      inner_data = LZSSDecompressor.decompress(io)
+      @compressed_size = io.pos
+      @ratio = inner_data.size.to_f / @compressed_size
 
-      inner_data = LZSSDecompressor.decompress(StringIO.new(data))
-
-      rowclass.new(inner_data, *rowargs)
+      super(rowclass.new(inner_data, *rowargs))
     end
+
+    attr_reader :compressed_size, :ratio
   end
 end
